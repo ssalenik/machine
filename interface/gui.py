992ce9836f32c7c1a2d6398c2497329272a0f34b
Stepan Salenikovich <stepan.salenikovich@gmail.com>
2012-12-01 21:32:27 -0500

    added claw and command widgets

--- interface/gui.py
+++ interface/gui.py
@@ -25,12 +25,14 @@ class CentralWidget(QWidget):
         # widgets which will be contained in the central widget
         self.settings = Settings()
         self.controls = ControlsFrame()
+        self.command = Commands()
 
         # layout
         self.layout = QVBoxLayout()
         self.layout.setSpacing(1)
         self.layout.addWidget(self.settings)
         self.layout.addWidget(self.controls)
+        self.layout.addWidget(self.command)
 
         self.setLayout(self.layout)
         
@@ -247,22 +249,78 @@ class ClawFrame(QFrame):
         self.hLine1= QFrame()
         self.hLine1.setFrameStyle(QFrame.HLine | QFrame.Sunken)
         self.otherLabel = QLabel("Other")
+        self.openButton = QPushButton("open")
+        self.openButton.setMinimumWidth(110)
+        self.closeButton = QPushButton("close")
+        self.closeButton.setMinimumWidth(110)
+        self.clawPowerLabel = QLabel("power:")
+        self.clawPowerInput = QLineEdit()
+        self.clawPowerInput.setMaximumWidth(100)
+        self.clawPowerSet = QPushButton("set")
+        self.clawPowerSet.setMinimumWidth(50)
+        self.raiseButton = QPushButton("raise")
+        self.raiseButton.setMinimumWidth(110)
+        self.lowerButton = QPushButton("lower")
+        self.lowerButton.setMinimumWidth(110)
+        self.heightPowerLabel = QLabel("power:")
+        self.heightPowerInput = QLineEdit()
+        self.heightPowerInput.setMaximumWidth(100)
+        self.heightPowerSet = QPushButton("set")
+        self.heightPowerSet.setMinimumWidth(50)
+
         self.hLine2= QFrame()
         self.hLine2.setFrameStyle(QFrame.HLine | QFrame.Sunken)
 
         # layout
         self.layout = QGridLayout()
-        self.layout.addWidget(self.label, 0, 0, 1, 2, Qt.AlignHCenter)
-        self.layout.addWidget(self.hLine1, 1, 0, 1, 2)
-        self.layout.addWidget(self.otherLabel, 3, 0, 1, 2, Qt.AlignHCenter)
-        self.layout.addWidget(self.hLine2, 4, 0, 1, 2)
+        self.layout.addWidget(self.label, 0, 0, 1, 4, Qt.AlignHCenter)
+        self.layout.addWidget(self.hLine1, 1, 0, 1, 4)
+        self.layout.addWidget(self.openButton, 2, 0, 1, 2, Qt.AlignHCenter)
+        self.layout.addWidget(self.closeButton, 2, 2, 1, 2, Qt.AlignHCenter)
+        self.layout.addWidget(self.clawPowerLabel, 3, 0)
+        self.layout.addWidget(self.clawPowerInput, 3, 1, 1, 2, Qt.AlignHCenter)
+        self.layout.addWidget(self.clawPowerSet, 3, 3)
+        self.layout.addWidget(self.raiseButton, 4, 0, 1, 4, Qt.AlignHCenter)
+        self.layout.addWidget(self.lowerButton, 5, 0, 1, 4, Qt.AlignHCenter)
+        self.layout.addWidget(self.heightPowerLabel, 6, 0)
+        self.layout.addWidget(self.heightPowerInput, 6, 1, 1, 2, Qt.AlignHCenter)
+        self.layout.addWidget(self.heightPowerSet, 6, 3)
+        self.layout.addWidget(self.otherLabel, 7, 0, 1, 4, Qt.AlignHCenter)
+        self.layout.addWidget(self.hLine2, 8, 0, 1, 4)
 
         # make row at the end stretch
-        self.layout.setRowStretch(2, 1)
-        self.layout.setRowStretch(5, 1)
+        self.layout.setRowStretch(9, 1)
 
         self.setLayout(self.layout)
 
+class Commands(QFrame):
+
+    def __init__(self, parent=None):
+        super(Commands, self).__init__(parent)
+
+        self.setFrameStyle(QFrame.StyledPanel | QFrame.Plain)
+        
+        # widgets
+        self.label = QLabel("Custom Command:")
+        #self.hLine = QFrame()
+        #self.hLine.setFrameStyle(QFrame.HLine | QFrame.Sunken)
+        self.commandInput = QLineEdit()
+        self.commandInput.setMinimumWidth(200)
+        self.sendButton = QPushButton("send")
+
+        # layout
+        self.layout = QGridLayout()
+        self.layout.addWidget(self.label, 0, 0)
+        self.layout.addWidget(self.commandInput, 0, 1)
+        self.layout.addWidget(self.sendButton, 0, 2)
+
+        # make middle column stretch
+        self.layout.setColumnStretch(3, 1)
+
+        # make last row stretch
+        self.layout.setRowStretch(1, 1)
+
+        self.setLayout(self.layout)
 
 if __name__ == '__main__':
     # create the app
