02ecc248735e12012f0c54928c0530d6910eb429
Stepan Salenikovich <stepan.salenikovich@gmail.com>
2012-12-08 09:56:40 -0500

    going with this implementation of serial for now

--- interface/serialComm.py
+++ interface/serialComm.py
@@ -3,92 +3,6 @@
 """
 contains definitions for serial communication delimiters, commands, etc
 """
-# new line, always ends all commands and requests
-NL = '\n'
-FEEDBACK = '>'
-DRIVER = 'p'
-
-class SerialComm:
-
-	def __init__(self, parent=None):
-
-		# init values
-		# NOTE : these are the actual values, not their corresponding serial codes
-		# we'll use this module to store the values for now
-		# TODO: decide whether there should be a serialComm class or not
-
-		# driver init values
-		self.power_left = 0
-		self.power_right = 0
-		self.power_both = 0
-		self.dir_left = 0
-		self.dir_right = 0
-		self.pid_toggle = False
-		self.speed_left = 0
-		self.speed_right = 0
-		self.speed_both= 0
-
-		# feedback init values
-		self.refresh_all = 0
-		self.encoder_left = 0
-		self.encoder_right = 0
-		self.sensor_left = 0
-		self.sensor_right = 0
-		self.encoder_base = 0
-		self.sensor_base = 0
-		self.encoder_arm = 0
-		self.encoder_claw = 0
-		self.encoder_claw_height = 0
-
-
-# serial send codes start here
-
-
-
-power_left = 23
-
-def powerLeft(value=None):
-	power_left = 23
-	if value :
-		power_left = value
-	else :
-		return hex(power_left)
-
-#dictionaries
-commands = TwoWayDict()
-feedback = TwoWayDict()
-
-commands[powerLeft] = 0x01
-
-
-
-# commands = {
-# 	power_left : 0x01,
-# 	power_right : 0x02,
-# 	power_both : 0x05,
-# 	dir_left : 0x03,
-# 	dir_right : 0x04,
-# 	pid_toggle : 0x10,
-# 	speed_left : 0x11,
-# 	speed_right : 0x12,
-# 	speed_both : 0x15
-# 	# there are more...
-# }
-
-# # feedback
-# feedback = {
-# 	refresh_all : 0x00,
-# 	encoder_left : 0x01,
-# 	encoder_right: 0x02,
-# 	sensor_left : 0x11,
-# 	sensor_right : 0x12,
-# 	encoder_base : 0x20,
-# 	sensor_base : 0x21,
-# 	encoder_arm : 0x22,
-# 	encoder_claw : 0x30,
-# 	encoder_claw_height : 0x31
-# 	# add more as needed
-# }
 
 class TwoWayDict(dict):
 	"""
@@ -103,3 +17,98 @@ class TwoWayDict(dict):
 	def __setitem__(self, key, value):
 		dict.__setitem__(self, key, value)
 		dict.__setitem__(self, value, key)
+
+# new line, always ends all commands and requests
+NL = '\n'
+FEEDBACK = '>'
+DRIVER = 'p'
+
+commands = {
+	'power_left' 	: chr(0x01),
+	'power_right' 	: chr(0x02),
+	'power_both' 	: chr(0x05),
+	'dir_left' 		: chr(0x03),
+	'dir_right' 	: chr(0x04),
+	'pid_toggle' 	: chr(0x10),
+	'speed_left' 	: chr(0x11),
+	'speed_right' 	: chr(0x12),
+	'speed_both'	: chr(0x15)
+	# there are more...
+}
+
+# feedback
+feedback = {
+	'refresh_all' 			: chr(0x00),
+	'encoder_left' 			: chr(0x01),
+	'encoder_right'			: chr(0x02),
+	'speed_left'			: chr(0x03),
+	'speed_right'			: chr(0x04),
+	'position_left'			: chr(0x05),
+	'position_right'		: chr(0x06),
+	'sensor_left' 			: chr(0x11),
+	'sensor_right' 			: chr(0x12),
+	'encoder_base' 			: chr(0x20),
+	'sensor_base' 			: chr(0x21),
+	'encoder_arm' 			: chr(0x22),
+	'encoder_claw' 			: chr(0x30),
+	'encoder_claw_height' 	: chr(0x31)
+	# add more as needed
+}
+
+
+# class SerialComm:
+
+# 	def __init__(self, parent=None):
+
+# 		# init values
+# 		# NOTE : these are the actual values, not their corresponding serial codes
+# 		# we'll use this module to store the values for now
+# 		# TODO: decide whether there should be a serialComm class or not
+
+# 		# driver init values
+# 		self.power_left = 0
+# 		self.power_right = 0
+# 		self.power_both = 0
+# 		self.dir_left = 0
+# 		self.dir_right = 0
+# 		self.pid_toggle = False
+# 		self.speed_left = 0
+# 		self.speed_right = 0
+# 		self.speed_both= 0
+
+# 		# feedback init values
+# 		self.refresh_all = 0
+# 		self.encoder_left = 0
+# 		self.encoder_right = 0
+# 		self.sensor_left = 0
+# 		self.sensor_right = 0
+# 		self.encoder_base = 0
+# 		self.sensor_base = 0
+# 		self.encoder_arm = 0
+# 		self.encoder_claw = 0
+# 		self.encoder_claw_height = 0
+
+
+# # serial send codes start here
+
+
+
+# power_left = 23
+
+# def powerLeft(value=None):
+# 	power_left = 23
+# 	if value :
+# 		power_left = value
+# 	else :
+# 		return hex(power_left)
+
+# #dictionaries
+# commands = TwoWayDict()
+# feedback = TwoWayDict()
+
+# commands[powerLeft] = 0x01
+
+
+
+
+
